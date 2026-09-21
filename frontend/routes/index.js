730d96a785f73e6dc4a1bec215ddb0fd984b7d86
Sara Derai <sara.derai@telecom-sudparis.eu>
2026-09-21 18:38:40 +0200

    fix:  error 404 for recipe page

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -29,7 +29,7 @@ router.get('/search', function(req, res, next) {
     const ingredients = recipe.ingredients.toLowerCase();
 
 
-    // Renvoie vrai si l'un contient l'autre
+    // Returns true if one contains the other
     return name.includes(q) || q.includes(name) || ingredients.includes(q); 
 
   });
@@ -41,9 +41,23 @@ router.get('/search', function(req, res, next) {
 
 /* GET recipe page (by ID). */
 router.get('/recipe/:id', function(req, res, next) {
-  const recipeId = req.params.id;
-  
-  res.render('recipe', { id: recipeId });
+  const recipeId = parseInt(req.params.id);
+
+  // Get all the recipes from data base
+  const recipes = [
+    {id:1, name: 'Lasagna', ingredients: '2 tomatoes \n 20cl of cream', instruction: 'cook the tomatos'},
+    {id:2, name: 'Pizza', ingredients: '200g of flour \n 100g of cheese', instruction: 'mix the flour...'},
+    {id:3, name: 'French fries', ingredients: '5 potatoes \n salt', instruction: 'cut the potatoes'}
+  ];
+
+// Find the recipe matching ID
+  const recipe = recipes.find(r => r.id === recipeId);
+
+ // Error: if non existing recipe id
+  if (!recipe) {
+    return res.status(404).send("Error: Cannot find recipe");
+  }
+  res.render('recipe', { recipe: recipe });
 });
 module.exports = router;
 
